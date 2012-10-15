b75da146b2da0def8cba8322c6a117c2d304ef1b
Nicolas Thauvin <nicolas.thauvin@dalibo.com>
2012-10-15 12:37:07 +0200

    new homepage
    Use a two-column dashboard as home page after login, instead of tabs

--- lib/Probe/Site.pm
+++ lib/Probe/Site.pm
@@ -5,27 +5,58 @@ use Data::Dumper;
 use Probe::Collector;
 
 sub home {
-  my $self = shift;
+    my $self = shift;
 
-  # get the list of all schema
-  my $dbh = $self->database;
-  my $sth = $dbh->prepare("SELECT id, set_name, description FROM results ORDER BY upload_time DESC");
-  $sth->execute();
-  my $sets = [ ];
-  while (my ($i, $s, $d) = $sth->fetchrow()) {
-      push @{$sets}, { id => $i, set => $s, desc => $d };
-  }
+    my $dbh = $self->database;
 
-  $sth->finish;
-  $dbh->commit;
-  $dbh->disconnect;
+    # Get the list of scripts
+    my $sth = $dbh->prepare(qq{SELECT id, script_name, description FROM scripts});
+    $sth->execute;
+    my $scripts = [ ];
+    while (my ($i, $n, $d) = $sth->fetchrow) {
+	push @{$scripts}, { id => $i, name => $n, desc => $d };
+    }
+    $sth->finish;
 
-  $self->stash(sets => $sets);
+    $self->stash(scripts => $scripts);
 
-  # set redirection for the remove form
-  $self->session->{origin} = 'home';
+    # Get the list of results
+    $sth = $dbh->prepare(qq{SELECT id, set_name, description FROM results ORDER BY upload_time DESC LIMIT 5});
+    $sth->execute();
+    my $sets = [ ];
+    while (my ($i, $s, $d) = $sth->fetchrow()) {
+	push @{$sets}, { id => $i, set => $s, desc => $d };
+    }
+    $sth->finish;
 
-  $self->render();
+    $self->stash(results => $sets);
+
+    # Get the list of reports
+    $sth = $dbh->prepare(qq{SELECT r.id, r.report_name, r.description,
+  string_agg(DISTINCT g.graph_name, '<br />' ORDER BY g.graph_name)
+FROM reports r
+  JOIN report_contents c ON (r.id = c.id_report)
+  JOIN graphs g ON (g.id = c.id_graph)
+GROUP BY r.id, r.report_name, r.description
+ORDER BY r.report_name LIMIT 5});
+    $sth->execute;
+
+    my $reports = [ ];
+    while (my ($i, $n, $d, $g) = $sth->fetchrow) {
+	push @{$reports}, { id => $i, name => $n, desc => $d, graphs => $g };
+    }
+    $sth->finish;
+
+    $self->stash(reports => $reports);
+
+    $dbh->commit;
+    $dbh->disconnect;
+
+
+    # set redirection for the remove form
+    $self->session->{origin} = 'home';
+
+    $self->render();
 }
 
 sub upload {
